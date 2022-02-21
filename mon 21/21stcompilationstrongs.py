d1bc0294abe3e585dae472c2b0692094bde33ec0
OchyBonnie <97603895+OchyBonnie@users.noreply.github.com>
2022-02-21 09:50:15 +0300

    Add files via upload

---
+++ mon 21/21stcompilationstrongs.py
@@ -0,0 +1,220 @@
+# Python Program for
+# Creation of String
+
+# Creating a String
+# with single Quotes
+String1 = 'Welcome to the Geeks World'
+print("String with the use of Single Quotes: ")
+print(String1)
+
+# Creating a String
+# with double Quotes
+String1 = "I'm a Geek"
+print("\nString with the use of Double Quotes: ")
+print(String1)
+
+# Creating a String
+# with triple Quotes
+String1 = '''I'm a Geek and I live in a world of "Geeks"'''
+print("\nString with the use of Triple Quotes: ")
+print(String1)
+
+# Creating String with triple
+# Quotes allows multiple lines
+String1 = '''Geeks
+			For
+			Life'''
+print("\nCreating a multiline String: ")
+print(String1)
+
+
+# Python Program to Access
+# characters of String
+
+String1 = "GeeksForGeeks"
+print("Initial String: ")
+print(String1)
+
+# Printing First character
+print("\nFirst character of String is: ")
+print(String1[0])
+
+# Printing Last character
+print("\nLast character of String is: ")
+print(String1[-1])
+
+# Python Program to
+# demonstrate String slicing
+
+# Creating a String
+String1 = "GeeksForGeeks"
+print("Initial String: ")
+print(String1)
+
+# Printing 3rd to 12th character
+print("\nSlicing characters from 3-12: ")
+print(String1[3:12])
+
+# Printing characters between
+# 3rd and 2nd last character
+print("\nSlicing characters between " +
+	"3rd and 2nd last character: ")
+print(String1[3:-2])
+
+# Python Program to Update
+# character of a String
+
+# Python Program to Update
+# entire String
+
+String1 = "Hello, I'm a Geek"
+print("Initial String: ")
+print(String1)
+
+# Updating a String
+String1 = "Welcome to the Geek World"
+print("\nUpdated String: ")
+print(String1)
+
+# Python Program to Delete
+# entire String
+
+String1 = "Hello, I'm a Geek"
+print("Initial String: ")
+print(String1)
+
+# Deleting a String
+# with the use of del
+del String1
+print("\nDeleting entire String: ")
+#print(String1)
+
+
+# Python Program for
+# Escape Sequencing
+# of String
+
+# Initial String
+String1 = '''I'm a "Geek"'''
+print("Initial String with use of Triple Quotes: ")
+print(String1)
+
+# Escaping Single Quote
+String1 = 'I\'m a "Geek"'
+print("\nEscaping Single Quote: ")
+print(String1)
+
+# Escaping Double Quotes
+String1 = "I'm a \"Geek\""
+print("\nEscaping Double Quotes: ")
+print(String1)
+
+# Printing Paths with the
+# use of Escape Sequences
+String1 = "C:\\Python\\Geeks\\"
+print("\nEscaping Backslashes: ")
+print(String1)
+
+# Printing Geeks in HEX
+String1 = "This is \x47\x65\x65\x6b\x73 in \x48\x45\x58"
+print("\nPrinting in HEX with the use of Escape Sequences: ")
+print(String1)
+
+# Using raw String to
+# ignore Escape Sequences
+String1 = r"This is \x47\x65\x65\x6b\x73 in \x48\x45\x58"
+print("\nPrinting Raw String in HEX Format: ")
+print(String1)
+
+# Python Program for
+# Formatting of Strings
+
+# Default order
+String1 = "{} {} {}".format('Geeks', 'For', 'Life')
+print("Print String in default order: ")
+print(String1)
+
+# Positional Formatting
+String1 = "{1} {0} {2}".format('Geeks', 'For', 'Life')
+print("\nPrint String in Positional order: ")
+print(String1)
+
+# Keyword Formatting
+String1 = "{l} {f} {g}".format(g='Geeks', f='For', l='Life')
+print("\nPrint String in order of Keywords: ")
+print(String1)
+
+# Formatting of Integers
+String1 = "{0:b}".format(16)
+print("\nBinary representation of 16 is ")
+print(String1)
+
+# Formatting of Floats
+String1 = "{0:e}".format(165.6458)
+print("\nExponent representation of 165.6458 is ")
+print(String1)
+
+# Rounding off Integers
+String1 = "{0:.2f}".format(1/6)
+print("\none-sixth is : ")
+print(String1)
+
+# String alignment
+String1 = "|{:<10}|{:^10}|{:>10}|".format('Geeks', 'for', 'Geeks')
+print("\nLeft, center and right alignment with Formatting: ")
+print(String1)
+
+# To demonstrate aligning of spaces
+String1 = "\n{0:^16} was founded in {1:<4}!".format("GeeksforGeeks", 2009)
+print(String1)
+# Concatenation of tuples
+Tuple1 = (0, 1, 2, 3)
+Tuple2 = ('Geeks', 'For', 'Geeks')
+
+Tuple3 = Tuple1 + Tuple2
+
+# Printing first Tuple
+print("Tuple 1: ")
+print(Tuple1)
+
+# Printing Second Tuple
+print("\nTuple2: ")
+print(Tuple2)
+
+# Printing Final Tuple
+print("\nTuples after Concatenation: ")
+print(Tuple3)
+
+# Python program to demonstrate
+# Creation of Set in Python
+
+# Creating a Set
+set1 = set()
+print("Initial blank Set: ")
+print(set1)
+
+# Creating a Set with
+# the use of a String
+set1 = set("GeeksForGeeks")
+print("\nSet with the use of String: ")
+print(set1)
+
+# Creating a Set with
+# the use of Constructor
+# (Using object to Store String)
+String = 'GeeksForGeeks'
+set1 = set(String)
+print("\nSet with the use of an Object: " )
+print(set1)
+
+# Creating a Set with
+# the use of a List
+set1 = set(["Geeks", "For", "Geeks"])
+print("\nSet with the use of List: ")
+print(set1)
+
+
+
+
+
+
